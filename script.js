408c89b4149a3763bb8a2a9cb338ab8fa0705d41
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-09-10 09:59:58 +0530

    Added hoisting question

--- script.js
+++ script.js
@@ -12,14 +12,31 @@
 
 // Follow Up question ->
 //
-function ABC() {
-    console.log(a, b, c);
-    var a = 10; const b = 10; const c = 10;
-}
+//
+// function ABC() {
+//     console.log(a, b, c);
+//     var a = 10; const b = 10; const c = 10;
+// }
 
-ABC();
+// ABC();
 
 // Answer: In the execution context of function ABC() we have {a:undefined} in memory creation phase,but before initialisation and
-// after declaration b and c will be in TDZ(Temporal Dead Zone) because of which even the memory for both of them is created 
-// but unlike var they are not intialised with undefined because of which js engine throws the error of reference error when 
+// after declaration b and c will be in TDZ(Temporal Dead Zone) because of which even the memory for both of them is created
+// but unlike var they are not intialised with undefined because of which js engine throws the error of reference error when
 // encounters b.
+
+// Q2: Based on implicit and explicit binding
+
+var obj = {
+    name: "Madhav",
+    greet: function () {
+        console.log("Good morning!", this.name);
+    }
+}
+var obj2 = {
+    name: "Vasu"
+}
+
+obj.greet.call(obj2);
+
+// Answer: .call(thisContext, arg1,arg2,...) provides this context to the function in this case provides name:"Vasu" to greet.
