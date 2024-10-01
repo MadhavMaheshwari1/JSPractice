85d634b0058a99d24b4c7d7f8f285ea9e508299f
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-10-01 09:38:29 +0530

    Learned about different data types in js

--- jsBasics.js
+++ jsBasics.js
@@ -1,3 +1,26 @@
-const num = Number("33abc")
-console.log(isNaN("33abc")); // Returns true
-console.log(isNaN({})); // Returns true
+// const num = Number("33abc")
+// console.log(isNaN("33abc")); // Returns true
+// console.log(isNaN({})); // Returns true
+// console.log(typeof "3.14".valueOf()); // Returns 3.14,doesn't perform type coercion
+// console.log([1, 2, 3] + ' is an array'); // "1,2,3 is an array" (array is coerced to string)
+// console.log({} + ''); // "[object Object]" (empty object is coerced to string)
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+// console.log(multiply('5', 2)); // 10 (string '5' is coerced to number)
+// console.log(Boolean([])); // true
+// console.log(Boolean({})); // true
+// console.log(+true); // 1
+
+// console.log(null >= 0);
+
+// let str = "Hello";
+// str[0] = "h";                // This does not work; strings are immutable
+// console.log(str);           // "Hello"
+
+// str = "h" + str.slice(1);   // This creates a new string
+// console.log(str);           // "hello"
+
+
+
