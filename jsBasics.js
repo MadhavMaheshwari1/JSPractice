8c8a6774466b96bebb881f8b087bc4424abc4e03
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-10-01 11:06:45 +0530

    Revisited critical differences b/w slice and splice function on arrays

--- jsBasics.js
+++ jsBasics.js
@@ -22,5 +22,40 @@
 // str = "h" + str.slice(1);   // This creates a new string
 // console.log(str);           // "hello"
 
+// const id = Symbol(1)
+// console.log(id);
+
+// const func = (a, b) => a * b
+// const arr = [234,23423,234324]
+// console.log(typeof func == 'function',typeof arr,typeof {},typeof [])
+
+// let str = new String("Madhav")
+// let str2 = "Madhav"
+// let val = str
+// val[0] = 'h'
+// console.log(val);
+
+// const min = 25
+// const max = 200
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+const arr = [234, 23423, 234324, 23234, 245695]
+// const newArr = arr.join('-')
+
+// console.log(arr);
+// console.log(newArr);
+
+// const newArr1 = arr.slice(0, arr.length - 2)
+// const newArr2 = arr.splice(0, arr.length - 2)
+// console.log(newArr1);
+// console.log(arr);
+// console.log(newArr2);
+
+
+
+
+
+
+
 
 
